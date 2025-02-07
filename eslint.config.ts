2e02b1c4a6a5ff39a87f279dcafa1877b01a8039
Achmad F. Ibrahim <acfatah@gmail.com>
2025-02-07 10:35:51 +0800

    feat: integrate @antfu/eslint-config and adjust ESLint setup
    - Add @antfu/eslint-config to devDependencies in package.json
    - Update ESLint script commands for linting and auto-fixing
    - Extend eslint.config.ts to include @antfu/eslint-config rules and configurations for improved linting across various file types

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,46 @@
+/* eslint-disable antfu/no-top-level-await */
+
 import pluginVue from 'eslint-plugin-vue'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVitest from '@vitest/eslint-plugin'
+import {
+  comments,
+  ignores,
+  imports,
+  javascript,
+  jsdoc,
+  jsonc,
+  markdown,
+  sortPackageJson,
+  sortTsconfig,
+  stylistic,
+  toml,
+  typescript,
+  unicorn,
+  vue,
+  yaml,
+} from '@antfu/eslint-config'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
 // configureVueProject({ scriptLangs: ['ts', 'tsx'] })
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+const antfuComments = await comments()
+const antfuIgnores = await ignores()
+const antfuImports = await imports()
+const antfuJavascript = await javascript()
+const antfuJsdoc = await jsdoc()
+const antfuJsonc = await jsonc()
+const antfuMarkdown = await markdown()
+const antfuPackageJson = await sortPackageJson()
+const antfuSortTsconfig = sortTsconfig()
+const antfuStylistic = await stylistic()
+const antfuToml = await toml()
+const antfuTypescript = await typescript()
+const antfuUnicorn = await unicorn()
+const antfuVue = await vue()
+const antfuYaml = await yaml()
 
 export default defineConfigWithVueTs(
   {
@@ -20,9 +55,25 @@ export default defineConfigWithVueTs(
 
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
+
+  ...antfuComments,
+  ...antfuIgnores,
+  ...antfuImports,
+  ...antfuJavascript,
+  ...antfuJsdoc,
+  ...antfuJsonc,
+  ...antfuMarkdown,
+  ...antfuPackageJson,
+  ...antfuSortTsconfig,
+  ...antfuStylistic,
+  ...antfuToml,
+  ...antfuTypescript,
+  ...antfuUnicorn,
+  ...antfuVue,
+  ...antfuYaml,
 )
