0dfd1b7ac02e2df40892df116e1af12291c9d023
Achmad F. Ibrahim <acfatah@gmail.com>
2025-02-10 09:04:44 +0800

    feat(vue-components): integrate unplugin-auto-import

---
+++ src/components/helpers/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Autoload any helper functions in this directory
+ *
+ * Any exported function within this directory will be available either in
+ * Vue script or template tags
+ *
+ * See: https://github.com/unplugin/unplugin-auto-import
+ */
--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,23 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import { resolvers, dirs as componentDirs } from './src/components/resolvers'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      dts: true,
+      vueTemplate: true,
+      dirs: [
+        'src/components/helpers',
+      ],
+      imports: [
+        'vue',
+        'vue-router',
+      ],
+    }),
     Components({
       dts: true,
       resolvers,
