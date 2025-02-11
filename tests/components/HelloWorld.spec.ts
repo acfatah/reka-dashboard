af784bec635d7d54a71b4a344b193f426e76f0c5
Achmad F. Ibrahim <acfatah@gmail.com>
2025-02-12 07:48:25 +0800

    refactor(tests): update test to set up Pinia store correctly

--- tests/components/HelloWorld.spec.ts
+++ tests/components/HelloWorld.spec.ts
@@ -1,10 +1,21 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
+import { createPinia, setActivePinia } from 'pinia'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+function setupTestComponent() {
+  const pinia = createPinia()
+
+  setActivePinia(pinia)
+
+  return { pinia }
+}
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
+    setupTestComponent()
+
     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
