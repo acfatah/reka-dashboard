0f391aac7f582d7bfcfe57c954ff5836c2a24b64
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-11 07:49:06 +0800

    feat: add AlertDialog components and stories

---
+++ src/components/ui/alert-dialog/AlertDialogCancel.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { AlertDialogCancelProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { AlertDialogCancel } from 'reka-ui'
+import { computed } from 'vue'
+
+import { buttonVariants } from '@/components/ui/button'
+
+const props = defineProps<AlertDialogCancelProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <AlertDialogCancel
+    v-bind="delegatedProps"
+    :class="cn(
+      buttonVariants({ variant: 'outline' }),
+      'mt-2 sm:mt-0',
+      props.class,
+    )"
+  >
+    <slot />
+  </AlertDialogCancel>
+</template>
---
+++ stories/ui/AlertDialog/AlertDialog.stories.ts
@@ -0,0 +1,62 @@
+import type { StoryObj } from '@storybook/vue3'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * A modal dialog that interrupts the user with important content and expects a response.
+ *
+ * It is modal that blocks the user from interacting with the parent page until it's closed.
+ * In other words, it's a persistent dialog.
+ *
+ * Primitive API Reference: https://www.reka-ui.com/docs/components/alert-dialog.html
+ *
+ * See also:
+ * - [Feedback/Dialog](?path=/docs/feedback-dialog--docs) component
+ */
+export default {
+  title: 'Feedback/Alert Dialog',
+  component: DefaultStory,
+  subcomponents: {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
