6407a202457b78e47e5388b03b7e7768470c5cf4
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-11 17:57:15 +0800

    feat: add Input component and stories

---
+++ src/components/ui/input/Input.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { useVModel } from '@vueuse/core'
+
+const props = defineProps<{
+  defaultValue?: string | number
+  modelValue?: string | number
+  class?: HTMLAttributes['class']
+}>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', payload: string | number): void
+}>()
+
+const modelValue = useVModel(props, 'modelValue', emits, {
+  passive: true,
+  defaultValue: props.defaultValue,
+})
+</script>
+
+<template>
+  <input
+    v-model="modelValue"
+    :class="cn(
+      'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 shadow-sm',
+      'file:border-0 file:bg-transparent file:text-sm file:font-medium',
+      'text-sm placeholder:text-muted-foreground',
+      'focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none',
+      'transition-colors',
+      'disabled:cursor-not-allowed disabled:opacity-50',
+      props.class,
+    )"
+  >
+</template>
---
+++ stories/ui/Input/text/TextInput.stories.ts
@@ -0,0 +1,169 @@
+import type { StoryObj } from '@storybook/vue3'
+import { Input } from '@/components/ui/input'
+import { Toaster } from '@/components/ui/toast'
+import { html } from 'common-tags'
+
+import FormStory from './FormStory.vue'
+import FormSource from './FormStory.vue?raw'
+import WithButtonStory from './WithButton.vue'
+import WithButtonSource from './WithButton.vue?raw'
+import WithIconStory from './WithIcon.vue'
+import WithIconSource from './WithIcon.vue?raw'
+import WithLabelStory from './WithLabel.vue'
+import WithLabelSource from './WithLabel.vue?raw'
+
+export default {
+  title: 'Data Entry/Text Input',
+  component: Input,
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: html`
+          <div class="flex flex-col w-full space-y-6 sm:w-2/3">
+            <Input type="text" placeholder="username" />
+          </div>
+        `,
+      },
+    },
+  },
+
+  args: {
+    placeholder: 'Username',
+    disabled: false,
+  },
+
+  render: args => ({
+    components: { Input },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <div class="flex flex-col w-full space-y-6 sm:w-2/3">
+        <Input v-bind="args" type="text" />
+      </div>
+    `,
+  }),
+}
+
+export const Form: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: FormSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { FormStory, Toaster },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <Teleport to="body">
+        <Toaster />
+      </Teleport>
+      <FormStory v-bind="args" />
+    `,
+  }),
+}
+
+export const Disabled: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: `
+<Input disabled />
+        `,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { Input },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <div class="w-full space-y-6 sm:w-2/3">
+        <Input v-bind="args" type="text" disabled placeholder="username" />
+      </div>
+    `,
+  }),
+}
+
+export const WithLabel: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: WithLabelSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { WithLabelStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <WithLabelStory v-bind="args" />
+    `,
+  }),
+}
+
+export const WithButton: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: WithButtonSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { WithButtonStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <WithButtonStory v-bind="args" />
+    `,
+  }),
+}
+
+export const WithIcon: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: WithIconSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { WithIconStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <WithIconStory v-bind="args" />
+    `,
+  }),
+}
