70753c9f569c5b570f3f7f37196fdfbd91fb56df
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-11 18:23:58 +0800

    feat: add Switch component and stories

---
+++ stories/ui/Switch/DefaultStory.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useForwardProps } from 'reka-ui'
+import { useAttrs } from 'vue'
+
+const forwardedProps = useForwardProps(useAttrs())
+</script>
+
+<template>
+  <div class="flex items-center space-x-2">
+    <Switch id="airplane-mode" v-bind="forwardedProps" />
+    <Label for="airplane-mode">Airplane Mode</Label>
+  </div>
+</template>
---
+++ stories/ui/Switch/Switch.stories.ts
@@ -0,0 +1,74 @@
+import type { StoryObj } from '@storybook/vue3'
+import { Switch } from '@/components/ui/switch'
+import { Toaster } from '@/components/ui/toast'
+import { html } from 'common-tags'
+
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+import FormStory from './FormStory.vue'
+import FormSource from './FormStory.vue?raw'
+
+/**
+ * A control that allows the user to toggle between checked and not checked.
+ *
+ * Primitive API Reference: https://www.reka-ui.com/docs/components/switch.html
+ */
+export default {
+  title: 'Data Entry/Switch',
+  component: DefaultStory,
+  subcomponents: {
+    Switch,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  args: {
+    disabled: false,
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
+
+export const Form: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: FormSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { FormStory, Toaster },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <Teleport to="body">
+        <Toaster />
+      </Teleport>
+      <FormStory v-bind="args" />
+    `,
+  }),
+}
