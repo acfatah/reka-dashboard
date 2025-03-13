d343e61634eb0a0e346aa12c5c1e9b1abdae49e3
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-13 08:04:05 +0800

    feat: add AspectRatio component and stories

---
+++ stories/ui/AspectRatio/AspectRatio.stories.ts
@@ -0,0 +1,41 @@
+import type { StoryObj } from '@storybook/vue3'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * Displays content within a desired ratio.
+ *
+ * Primitive API Reference: https://www.reka-ui.com/docs/components/aspect-ratio.html
+ */
+export default {
+  title: 'Graphic Elements/Aspect Ratio',
+  component: DefaultStory,
+  subcomponents: {
+    AspectRatio,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
