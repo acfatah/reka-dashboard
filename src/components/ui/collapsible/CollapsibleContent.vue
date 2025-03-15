2135abfac39ba174d2080915d96da6788667f0a3
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-15 17:53:50 +0800

    feat: add Collapsible components and stories

---
+++ src/components/ui/collapsible/CollapsibleContent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { CollapsibleContentProps } from 'reka-ui'
+import { cn } from '@/lib/utils'
+import { CollapsibleContent } from 'reka-ui'
+
+const props = defineProps<CollapsibleContentProps>()
+</script>
+
+<template>
+  <CollapsibleContent
+    v-bind="props"
+    :class="cn(
+      'overflow-hidden transition-all',
+      'data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down',
+    )"
+  >
+    <slot />
+  </CollapsibleContent>
+</template>
