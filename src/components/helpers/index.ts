58ee3e43c1d588aeb8a2d6e19759c72e2138a983
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-15 18:35:07 +0800

    feat: autoload `cn` utils using helpers

--- src/components/helpers/index.ts
+++ src/components/helpers/index.ts
@@ -2,7 +2,9 @@
  * Autoload any helper functions in this directory
  *
  * Any exported function within this directory will be available either in
- * Vue script or template tags
+ * Vue script or template tags including their types.
  *
  * See: https://github.com/unplugin/unplugin-auto-import
  */
+
+export { cn } from '@/lib/utils'
