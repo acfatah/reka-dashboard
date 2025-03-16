8bd81d6cdcfdf1a8829563fe9b6df5416e5d1d12
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-16 09:07:01 +0800

    feat: add Form components and stories

---
+++ src/components/ui/form/FormAction.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  justifyEnd?: boolean
+  class?: HTMLAttributes['class']
+}>()
+
+const delegatedProps = computed(() => {
+  const { class: _, justifyEnd, ...delegated } = props
+
+  return { ...delegated }
+})
+</script>
+
+<template>
+  <div
+    v-bind="delegatedProps"
+    :class="cn(
+      'flex gap-2 mt-6',
+      [props.justifyEnd && 'flex-row-reverse'],
+      props.class,
+    )"
+  >
+    <slot />
+  </div>
+</template>
---
+++ stories/ui/Form/Form.stories.ts
@@ -0,0 +1,66 @@
+import type { StoryObj } from '@storybook/vue3'
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormFieldArray,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Toaster } from '@/components/ui/toast'
+import { html } from 'common-tags'
+
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * Form handling using Vee Validate.
+ *
+ * Primitive API Reference: https://vee-validate.logaretm.com/v4/guide/components/handling-forms
+ */
+export default {
+  title: 'Forms/Form',
+  component: DefaultStory,
+  subcomponents: {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormFieldArray,
+    FormItem,
+    FormLabel,
+    FormMessage,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  args: {
+    disabled: false,
+  },
+
+  render: args => ({
+    components: { DefaultStory, Toaster },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <Teleport to="body">
+        <Toaster />
+      </Teleport>
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
