37d1909034dab1ce7859227d566d224014ad3a53
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-16 09:05:18 +0800

    feat: add PasswordInput component and stories

---
+++ stories/ui/Input/password/FormStory.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import type { GenericObject, SubmissionHandler } from 'vee-validate'
+import { toast } from '@/components/ui/toast'
+import { toTypedSchema } from '@vee-validate/zod'
+import { h } from 'vue'
+import { z } from 'zod'
+
+const schema = z.object({
+  password: z
+    .string()
+    .min(4, {
+      message: 'Password must be at least 4 characters.',
+    }),
+})
+
+const formSchema = toTypedSchema(schema)
+
+const onSubmit: SubmissionHandler<GenericObject> = function (values) {
+  const formValues = values as z.infer<typeof schema>
+
+  toast({
+    title: 'You submitted the following values:',
+    description: h(
+      'pre',
+      { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' },
+      h('code', { class: 'text-white' }, JSON.stringify(formValues, null, 2)),
+    ),
+  })
+}
+</script>
+
+<template>
+  <Form
+    class="flex flex-col w-full space-y-6 sm:w-2/3"
+    :validation-schema="formSchema"
+    @submit="onSubmit"
+  >
+    <FormField v-slot="{ componentField }" name="password">
+      <FormItem>
+        <FormLabel>Password</FormLabel>
+        <FormControl>
+          <PasswordInput v-bind="componentField" placeholder="Enter password" />
+        </FormControl>
+        <FormDescription>
+          You login password
+        </FormDescription>
+        <FormMessage />
+      </FormItem>
+    </FormField>
+
+    <Button type="submit">
+      Submit
+    </Button>
+  </Form>
+</template>
---
+++ stories/ui/Input/password/InputPassword.stories.ts
@@ -0,0 +1,73 @@
+import type { StoryObj } from '@storybook/vue3'
+import { PasswordInput } from '@/components/ui/password-input'
+import { Toaster } from '@/components/ui/toast'
+import { html } from 'common-tags'
+
+import FormStory from './FormStory.vue'
+import FormSource from './FormStory.vue?raw'
+
+export default {
+  title: 'Data Entry/Password Input',
+  component: PasswordInput,
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: html`
+          <PasswordInput placeholder="Enter password" />
+        `,
+      },
+    },
+  },
+
+  args: {
+    placeholder: 'Enter password',
+    disabled: false,
+  },
+
+  render: args => ({
+    components: { PasswordInput },
+
+    setup() {
+      const password = 'mY$eRcR3tP@ssw0rd'
+
+      return { args, password }
+    },
+
+    template: html`
+      <div class="flex flex-col w-full space-y-6 sm:w-2/3">
+        <PasswordInput v-bind="args" />
+      </div>
+    `,
+  }),
+}
+
+export const Form: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: FormSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { FormStory, Toaster },
+
+    setup() {
+      const password = 'mY$eRcR3tP@ssw0rd'
+
+      return { args, password }
+    },
+
+    template: html`
+      <Teleport to="body">
+        <Toaster />
+      </Teleport>
+      <FormStory v-bind="args" />
+    `,
+  }),
+}
