80d76f8804cb72004be2678236dc752cb3196722
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-16 12:37:35 +0800

    feat: add DatePicker stories

---
+++ stories/ui/DatePicker/DatePicker.stories.ts
@@ -0,0 +1,103 @@
+import type { StoryObj } from '@storybook/vue3'
+import { Calendar } from '@/components/ui/calendar'
+import { Toaster } from '@/components/ui/toast'
+import { html } from 'common-tags'
+
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+import FormStory from './FormStory.vue'
+import FormSource from './FormStory.vue?raw'
+import WithPresetStory from './WithPresetStory.vue'
+import WithPresetSource from './WithPresetStory.vue?raw'
+
+/**
+ * A date field component that allows users to enter and edit date.
+ *
+ * The Date Picker is built using a composition of the `<Popover />` and
+ * either the `<Calendar />` or `<RangeCalendar />` components.
+ *
+ * Documentation: https://www.shadcn-vue.com/docs/components/date-picker.html<br>
+ * Guide: https://reka-ui.com/docs/guides/dates.html<br>
+ * Primitive API Reference: https://reka-ui.com/docs/components/calendar.html
+ *
+ * See also:
+ * - [Overlays/Popover](?path=/docs/overlays-popover--docs) component
+ * - [Data Display/Calendar](?path=/docs/data-display-calendar--docs) component
+ * - [Data Display/Range Calendar](?path=/docs/data-display-range-calendar--docs) component
+ * - [Data Entry/Date Range Picker](?path=/docs/data-entry-date-range-picker--docs) component
+ * - [@internationalized/date](https://react-spectrum.adobe.com/internationalized/date/index.html)
+ */
+export default {
+  title: 'Data Entry/Date Picker',
+  component: Calendar,
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
+
+export const Form: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: FormSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { FormStory, Toaster },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <Teleport to="body">
+        <Toaster />
+      </Teleport>
+      <FormStory v-bind="args" />
+    `,
+  }),
+}
+
+export const WithPreset: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: WithPresetSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { WithPresetStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <WithPresetStory v-bind="args" />
+    `,
+  }),
+}
---
+++ stories/ui/DatePicker/DefaultStory.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { DateValue } from '@internationalized/date'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Iconify } from '@/components/ui/icon'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+import {
+  DateFormatter,
+  getLocalTimeZone,
+} from '@internationalized/date'
+import { ref } from 'vue'
+
+const df = new DateFormatter('en-US', {
+  dateStyle: 'long',
+})
+
+const value = ref<DateValue>()
+</script>
+
+<template>
+  <Popover>
+    <PopoverTrigger as-child>
+      <Button
+        variant="outline"
+        :class="cn(
+          'w-[280px] justify-start text-left font-normal',
+          !value && 'text-muted-foreground',
+        )"
+      >
+        <Iconify icon="lucide:calendar" class="mr-2 size-4" />
+        {{ value ? df.format(value.toDate(getLocalTimeZone())) : "Pick a date" }}
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent class="w-auto p-0">
+      <Calendar v-model="value" initial-focus />
+    </PopoverContent>
+  </Popover>
+</template>
