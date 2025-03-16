c9cbd1c9c3d6a94bf906b817e2523a15c9298b6f
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-16 12:38:05 +0800

    feat: add DateRangePicker stories

---
+++ stories/ui/DateRangePicker/DateRangePicker.stories.ts
@@ -0,0 +1,126 @@
+import type { StoryObj } from '@storybook/vue3'
+import {
+  RangeCalendar,
+  RangeCalendarCell,
+  RangeCalendarCellTrigger,
+  RangeCalendarGrid,
+  RangeCalendarGridBody,
+  RangeCalendarGridHead,
+  RangeCalendarGridRow,
+  RangeCalendarHeadCell,
+  RangeCalendarHeader,
+  RangeCalendarHeading,
+  RangeCalendarNextButton,
+  RangeCalendarPrevButton,
+} from '@/components/ui/range-calendar'
+import { Toaster } from '@/components/ui/toast'
+import { html } from 'common-tags'
+
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+import FormStory from './FormStory.vue'
+import FormSource from './FormStory.vue?raw'
+import WithIndependentMonthsStory from './WithIndependentMonths.vue'
+import WithIndependentMonthsSource from './WithIndependentMonths.vue?raw'
+
+/**
+ * A calendar component that allows users to select a range of dates.
+ *
+ * Documentation: https://www.shadcn-vue.com/docs/components/date-picker.html<br>
+ * Guide: https://reka-ui.com/docs/guides/dates.html<br>
+ * Primitive API Reference: https://reka-ui.com/docs/components/date-range-picker
+ *
+ * See also:
+ * - [Data Display/Calendar](?path=/docs/data-display-calendar--docs) component
+ * - [Data Display/Range Calendar](?path=/docs/data-display-range-calendar--docs) component
+ * - [Data Entry/Date Picker](?path=/docs/data-entry-date-picker--docs) component
+ * - [@internationalized/date](https://react-spectrum.adobe.com/internationalized/date/index.html)
+ */
+export default {
+  title: 'Data Entry/Date Range Picker',
+  component: DefaultStory,
+  subcomponents: {
+    RangeCalendar,
+    RangeCalendarCell,
+    RangeCalendarCellTrigger,
+    RangeCalendarGrid,
+    RangeCalendarGridBody,
+    RangeCalendarGridHead,
+    RangeCalendarGridRow,
+    RangeCalendarHeadCell,
+    RangeCalendarHeader,
+    RangeCalendarHeading,
+    RangeCalendarNextButton,
+    RangeCalendarPrevButton,
+  },
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
+
+export const Form: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: FormSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { FormStory, Toaster },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <Teleport to="body">
+        <Toaster />
+      </Teleport>
+      <FormStory v-bind="args" />
+    `,
+  }),
+}
+
+export const WithIndependentMonths: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: WithIndependentMonthsSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { WithIndependentMonthsStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <WithIndependentMonthsStory v-bind="args" />
+    `,
+  }),
+}
---
+++ stories/ui/DateRangePicker/DefaultStory.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { DateRange } from 'reka-ui'
+import type { Ref } from 'vue'
+import { RangeCalendar } from '@/components/ui/range-calendar'
+import { getLocalTimeZone, today } from '@internationalized/date'
+import { ref } from 'vue'
+
+const start = today(getLocalTimeZone())
+const end = start.add({ days: 7 })
+
+const value = ref({
+  start,
+  end,
+}) as Ref<DateRange>
+</script>
+
+<template>
+  <RangeCalendar v-model="value" class="w-fit rounded-md border" />
+</template>
