b86b9dc68c43d22fbd2fc679c8d28a238f613dcb
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-16 12:39:37 +0800

    feat: autoload reka-ui date utility functions

--- src/components/helpers/index.ts
+++ src/components/helpers/index.ts
@@ -8,3 +8,27 @@
  */
 
 export { cn } from '@/lib/utils'
+
+// https://react-spectrum.adobe.com/internationalized/date/index.html
+export { CalendarDate, type DateValue } from '@internationalized/date'
+
+// https://reka-ui.com/docs/guides/dates
+export {
+  createDateRange,
+  createDecade,
+  createMonth,
+  createYear,
+  createYearRange,
+  getDaysInMonth,
+  hasTime,
+  isAfter,
+  isAfterOrSame,
+  isBefore,
+  isBeforeOrSame,
+  isBetween,
+  isBetweenInclusive,
+  isCalendarDateTime,
+  isZonedDateTime,
+  parseStringToDateValue,
+  toDate,
+} from 'reka-ui/date'
