1b2fe2c316a1c150e67897a0967532562ded6ada
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-16 11:31:59 +0800

    feat: add RangeCalendar components and stories

---
+++ src/components/ui/range-calendar/RangeCalendarHeading.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import type { RangeCalendarHeadingProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { RangeCalendarHeading, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+
+const props = defineProps<RangeCalendarHeadingProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <RangeCalendarHeading
+    v-slot="{ headingValue }"
+    :class="cn('text-sm font-medium', props.class)"
+    v-bind="forwardedProps"
+  >
+    <slot :heading-value>
+      {{ headingValue }}
+    </slot>
+  </RangeCalendarHeading>
+</template>
---
+++ stories/ui/RangeCalendar/RangeCalendar.stories.ts
@@ -0,0 +1,48 @@
+import type { StoryObj } from '@storybook/vue3'
+import { Label } from '@/components/ui/label'
+import { html } from 'common-tags'
+
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * A calendar component that allows users to select a range of dates.
+ *
+ * The `<RangeCalendar />` component is built on top of the `reka-ui` `Range Calendar`
+ * component, which uses the `@internationalized/date` package to handle dates.
+ *
+ * Documentation: https://www.shadcn-vue.com/docs/components/range-calendar.html<br>
+ * Guide: https://reka-ui.com/docs/guides/dates.html<br>
+ * Primitive API Reference: https://reka-ui.com/docs/components/date-range-picker
+ *
+ * See also:
+ * - [Data Display/Calendar](?path=/docs/data-display-calendar--docs) component
+ * - [@internationalized/date](https://react-spectrum.adobe.com/internationalized/date/index.html)
+ */
+export default {
+  title: 'Data Display/Range Calendar',
+  component: Label,
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: html`
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
