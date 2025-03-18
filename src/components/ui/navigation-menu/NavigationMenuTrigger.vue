74c56615be468a944dd3f298299430d8c02705c5
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-19 07:06:07 +0800

    feat: add NavigationMenu components and stories

---
+++ src/components/ui/navigation-menu/NavigationMenuTrigger.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { NavigationMenuTriggerProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { Iconify } from '@/components/ui/icon'
+import { cn } from '@/lib/utils'
+import { NavigationMenuTrigger, useForwardProps } from 'reka-ui'
+import { computed } from 'vue'
+import { navigationMenuTriggerStyle } from '.'
+
+const props = defineProps<NavigationMenuTriggerProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwardedProps = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <NavigationMenuTrigger
+    v-bind="forwardedProps"
+    :class="cn(navigationMenuTriggerStyle(), 'group', props.class)"
+  >
+    <slot />
+    <Iconify icon="lucide:chevron-down" class="relative top-px ml-1 size-3 transition duration-300 group-data-[state=open]:rotate-180" />
+  </NavigationMenuTrigger>
+</template>
