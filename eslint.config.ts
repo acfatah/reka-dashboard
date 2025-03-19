dc74e275ce32fbab589adbbdcb3651e81d1055ae
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-19 21:52:24 +0800

    refactor(eslint): use antfu config preset

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,51 +1,20 @@
-/* eslint-disable antfu/no-top-level-await */
-
-import pluginVue from 'eslint-plugin-vue'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import antfu from '@antfu/eslint-config'
 import pluginVitest from '@vitest/eslint-plugin'
-import {
-  comments,
-  ignores,
-  imports,
-  javascript,
-  jsdoc,
-  jsonc,
-  markdown,
-  sortPackageJson,
-  sortTsconfig,
-  stylistic,
-  toml,
-  typescript,
-  unicorn,
-  vue,
-  yaml,
-} from '@antfu/eslint-config'
 
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+export default antfu(
+  {},
 
-const antfuComments = await comments()
-const antfuIgnores = await ignores()
-const antfuImports = await imports()
-const antfuJavascript = await javascript()
-const antfuJsdoc = await jsdoc()
-const antfuJsonc = await jsonc()
-const antfuMarkdown = await markdown()
-const antfuPackageJson = await sortPackageJson()
-const antfuSortTsconfig = sortTsconfig()
-const antfuStylistic = await stylistic()
-const antfuToml = await toml()
-const antfuTypescript = await typescript()
-const antfuUnicorn = await unicorn()
-const antfuVue = await vue()
-const antfuYaml = await yaml()
-
-export default defineConfigWithVueTs(
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    rules: {
+      'sort-imports': 'off',
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          partitionByNewLine: true,
+          newlinesBetween: 'ignore',
+        },
+      ],
+    },
   },
 
   {
@@ -53,38 +22,8 @@ export default defineConfigWithVueTs(
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'logs'],
   },
 
-  pluginVue.configs['flat/essential'],
-  vueTsConfigs.recommended,
-
   {
-    ...pluginVitest.configs.recommended,
+    rules: pluginVitest.configs.recommended.rules,
     files: ['src/**/__tests__/*', 'tests/**/*'],
-  },
-
-  ...antfuComments,
-  ...antfuIgnores,
-  ...antfuImports,
-  ...antfuJavascript,
-  ...antfuJsdoc,
-  ...antfuJsonc,
-  ...antfuMarkdown,
-  ...antfuPackageJson,
-  ...antfuSortTsconfig,
-  ...antfuStylistic,
-  ...antfuToml,
-  ...antfuTypescript,
-  ...antfuUnicorn,
-  ...antfuVue,
-  ...antfuYaml,
-
-  // Need the following lines in the correct order.
-  // See: https://github.com/eslint/eslint/discussions/17221#discussioncomment-6028301
-  {
-    files: ['*.vue', '**/*.vue'],
-    languageOptions: {
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-      },
-    },
   },
 )
