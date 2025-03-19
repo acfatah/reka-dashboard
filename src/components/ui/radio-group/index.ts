3bb223a28c19e19dbf444a1ed1baa26d33011bef
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-19 21:33:37 +0800

    refactor(RadioGroup): remove unused context creation

--- src/components/ui/radio-group/index.ts
+++ src/components/ui/radio-group/index.ts
@@ -1,9 +1,2 @@
-import type { Ref } from 'vue'
-import { createContext } from 'reka-ui'
-
 export { default as RadioGroup } from './RadioGroup.vue'
 export { default as RadioGroupItem } from './RadioGroupItem.vue'
-
-export const [useRadioGroup, provideRadioGroupContext] = createContext<{
-  disabled: Ref<boolean>
-}>('RadioGroup')
