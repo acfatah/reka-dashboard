1c05ae203ab144eb163e1d317e0a37ece4a88fef
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-21 12:29:06 +0800

    feat: add Progress component and stories

---
+++ stories/ui/Progress/Progress.stories.ts
@@ -0,0 +1,39 @@
+import type { StoryObj } from '@storybook/vue3'
+
+import { Progress } from '@/components/ui/progress'
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * Displays an indicator showing the completion progress of a task, typically displayed
+ * as a progress bar.
+ *
+ * Primitive API Reference: https://reka-ui.com/docs/components/progress
+ */
+export default {
+  title: 'Feedback/Progress',
+  component: Progress,
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
