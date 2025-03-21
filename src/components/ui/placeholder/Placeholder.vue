b8854884624a1f0991e072a7b5039e0167204084
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-21 12:16:07 +0800

    feat: add Placeholder components and stories

---
+++ src/components/ui/placeholder/Placeholder.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { CxOptions } from 'class-variance-authority'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{ class?: CxOptions }>()
+</script>
+
+<template>
+  <div
+    :class="cn(
+      'relative flex items-center justify-center overflow-hidden rounded-md border text-muted-foreground',
+      props.class,
+    )"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="absolute inset-0 size-full stroke-neutral-400/30 dark:stroke-neutral-500/40"
+    >
+      <defs>
+        <pattern id="placeholder-svg-pattern" patternUnits="userSpaceOnUse" width="10" height="10">
+          <path d="M-3 13 15-5M-5 5l18-18M-1 21 17 3" />
+        </pattern>
+      </defs>
+      <rect width="100%" height="100%" fill="url(#placeholder-svg-pattern)" />
+    </svg>
+    <slot />
+  </div>
+</template>
---
+++ stories/ui/Placeholder/Placeholder.stories.ts
@@ -0,0 +1,36 @@
+import type { StoryObj } from '@storybook/vue3'
+
+import { Placeholder } from '@/components/ui/placeholder'
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * Temporary element as a placeholder for content.
+ */
+export default {
+  title: 'Common/Placeholder',
+  component: Placeholder,
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
