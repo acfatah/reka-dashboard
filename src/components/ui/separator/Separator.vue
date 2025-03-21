7b83bddb07ed0dd0aa9db43cf255ba5e910cc127
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-22 07:47:46 +0800

    feat: add Separator component and stories

---
+++ src/components/ui/separator/Separator.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { SeparatorProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { Separator } from 'reka-ui'
+import { computed } from 'vue'
+
+const props = defineProps<
+  SeparatorProps & { class?: HTMLAttributes['class'], label?: string }
+>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <Separator
+    v-bind="delegatedProps"
+    :class="
+      cn(
+        'shrink-0 bg-border relative',
+        props.orientation === 'vertical' ? 'w-px h-full' : 'h-px w-full',
+        props.class,
+      )
+    "
+  >
+    <span
+      v-if="props.label"
+      :class="
+        cn(
+          'absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 items-center justify-center',
+          'bg-background text-xs text-muted-foreground',
+          props.orientation === 'vertical' ? 'w-[1px] px-1 py-2' : 'h-[1px] py-1 px-2',
+        )
+      "
+    >{{ props.label }}</span>
+  </Separator>
+</template>
---
+++ stories/ui/Separator/Separator.stories.ts
@@ -0,0 +1,38 @@
+import type { StoryObj } from '@storybook/vue3'
+import { Separator } from '@/components/ui/separator'
+
+import DefaultStory from './DefaultStory.vue'
+import DefaultSource from './DefaultStory.vue?raw'
+
+/**
+ * Visually or semantically separates content.
+ *
+ * Primitive API Reference: https://reka-ui.com/docs/components/separator
+ */
+export default {
+  title: 'Panels/Separator',
+  component: Separator,
+  tags: ['autodocs'],
+}
+
+export const Default: StoryObj = {
+  parameters: {
+    docs: {
+      source: {
+        code: DefaultSource,
+      },
+    },
+  },
+
+  render: args => ({
+    components: { DefaultStory },
+
+    setup() {
+      return { args }
+    },
+
+    template: `
+      <DefaultStory v-bind="args" />
+    `,
+  }),
+}
