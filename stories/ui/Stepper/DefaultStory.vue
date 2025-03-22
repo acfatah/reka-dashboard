9461fd478e926e0120571a729a0b7b9d6be709f0
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-22 18:42:36 +0800

    feat: add Stepper components and stories

---
+++ stories/ui/Stepper/DefaultStory.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import type { VNode } from 'vue'
+import { Iconify } from '@/components/ui/icon'
+import { h } from 'vue'
+
+interface Step {
+  step: number
+  title: string
+  description: string
+  icon?: VNode
+}
+
+const steps: Step[] = [
+  {
+    step: 1,
+    title: 'Address',
+    description: 'Add your address here',
+    icon: h(Iconify, { icon: 'lucide:book-user', class: 'size-4' }),
+  },
+  {
+    step: 2,
+    title: 'Shipping',
+    description: 'Set your preferred shipping method',
+    icon: h(Iconify, { icon: 'lucide:truck', class: 'size-4' }),
+  },
+  {
+    step: 3,
+    title: 'Payment',
+    description: 'Add any payment information you have',
+    icon: h(Iconify, { icon: 'lucide:credit-card', class: 'size-4' }),
+  },
+  {
+    step: 4,
+    title: 'Checkout',
+    description: 'Confirm your order',
+    icon: h(Iconify, { icon: 'lucide:check', class: 'size-4' }),
+  },
+]
+</script>
+
+<template>
+  <Stepper>
+    <StepperItem
+      v-for="item in steps"
+      :key="item.step"
+      class="basis-1/4"
+      :step="item.step"
+    >
+      <StepperTrigger>
+        <StepperIndicator>
+          <component :is="item.icon" class="size-4" />
+        </StepperIndicator>
+        <div class="flex flex-col">
+          <StepperTitle>
+            {{ item.title }}
+          </StepperTitle>
+          <StepperDescription>
+            {{ item.description }}
+          </StepperDescription>
+        </div>
+      </StepperTrigger>
+      <StepperSeparator
+        v-if="item.step !== steps[steps.length - 1].step"
+        class="h-px w-full"
+      />
+    </StepperItem>
+  </Stepper>
+</template>
