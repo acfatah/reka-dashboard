744521bf32f2e7d1273f21df94bf358182646402
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-25 08:34:09 +0800

    feat: add BarChart components and stories

---
+++ stories/ui/BarChart/BarChart.stories.ts
@@ -0,0 +1,46 @@
+import { BarChart } from '@/components/ui/bar-chart/index.ts'
+import { monthlyData, monthlyPredictions } from './data.ts'
+import BarChartSource from './DefaultSnippet.vue?raw'
+import WithCategoriesSource from './WithCategorySnippet.vue?raw'
+
+export default {
+  title: 'Charts/Bar Chart',
+  component: BarChart,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      source: {
+        code: BarChartSource,
+      },
+    },
+  },
+}
+
+export const Default = {
+  args: {
+    data: monthlyData,
+    index: 'name',
+    categories: ['total'],
+  },
+}
+
+export const WithCategories = {
+  args: {
+    'data': monthlyPredictions,
+    'index': 'name',
+    'categories': ['total', 'predicted'],
+    'y-formatter': (tick: number | Date, _i: number) => {
+      return typeof tick === 'number'
+        ? `$ ${new Intl.NumberFormat('us').format(tick).toString()}`
+        : ''
+    },
+  },
+
+  parameters: {
+    docs: {
+      source: {
+        code: WithCategoriesSource,
+      },
+    },
+  },
+}
---
+++ stories/ui/BarChart/WithCategorySnippet.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const data = [
+  {
+    name: 'Jan',
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  // ...
+]
+</script>
+
+<template>
+  <BarChart
+    :data="data"
+    index="name"
+    :categories="['total', 'predicted']"
+    :y-formatter="(tick: number | Date, _i: number) => {
+      return typeof tick === 'number'
+        ? `$ ${new Intl.NumberFormat('us').format(tick).toString()}`
+        : ''
+    }"
+  />
+</template>
