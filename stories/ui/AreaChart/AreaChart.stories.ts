0d1edd086ad99b0ff787d3a34a3d4ccc6b718238
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-25 08:54:04 +0800

    feat: add AreaChart components and stories

---
+++ stories/ui/AreaChart/AreaChart.stories.ts
@@ -0,0 +1,49 @@
+import { AreaChart } from '@/components/ui/chart-area'
+import { CurveType } from '@unovis/ts'
+import data from '../LineChart/data/montly.js'
+import AreaChartSource from './DefaultSnippet.vue?raw'
+import SparklineSource from './SparklineSnippet.vue?raw'
+
+export default {
+  title: 'Charts/Area Chart',
+  component: AreaChart,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  args: {
+    data,
+    index: 'name',
+    categories: ['total', 'predicted'],
+  },
+
+  parameters: {
+    docs: {
+      source: {
+        code: AreaChartSource,
+      },
+    },
+  },
+}
+
+export const Sparkline = {
+  args: {
+    data,
+    'class': 'h-[100px] w-[400px]',
+    'index': 'name',
+    'categories': ['total'],
+    'show-grid-line': false,
+    'show-legend': false,
+    'show-x-axis': false,
+    'show-y-axis': false,
+    'curve-type': CurveType.Linear,
+  },
+
+  parameters: {
+    docs: {
+      source: {
+        code: SparklineSource,
+      },
+    },
+  },
+}
---
+++ stories/ui/AreaChart/SparklineSnippet.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { CurveType } from '@unovis/ts'
+
+const data = [
+  {
+    name: 'Jan',
+    total: Math.floor(Math.random() * 2000) + 500,
+    predicted: Math.floor(Math.random() * 2000) + 500,
+  },
+  // ...
+]
+</script>
+
+<template>
+  <AreaChart
+    class="h-[100px] w-[400px]"
+    index="name"
+    :data="data"
+    :categories="['total']"
+    :show-grid-line="false"
+    :show-legend="false"
+    :show-x-axis="false"
+    :show-y-axis="false"
+    :curve-type="CurveType.Linear"
+  />
+</template>
