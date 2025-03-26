2c07d4cd8562a5298273eb24cbed571e8ac78742
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-26 09:23:08 +0800

    feat: add foundation/spacing stories

---
+++ .storybook/components/Table.jsx
@@ -0,0 +1,39 @@
+// eslint-disable-next-line unused-imports/no-unused-imports, no-unused-vars
+import React from 'react'
+
+function Table({ headers, rows }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {Object.keys(row).map((cell, cellIndex) => (
+              <td key={cellIndex}>
+                {row[cell]}
+              </td>
+            ))}
+            <td>
+              <div
+                style={{
+                  minHeight: '24px',
+                  width: `${row.size}`,
+                  backgroundColor: 'green',
+                }}
+              >
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+export default Table
