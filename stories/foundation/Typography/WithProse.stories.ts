df5ecfed30a62339b4fea27f073d0da29e29c8c4
Achmad F. Ibrahim <acfatah@gmail.com>
2025-03-26 09:49:10 +0800

    add: foundation typography stories

---
+++ stories/foundation/Typography/WithProse.stories.ts
@@ -0,0 +1,27 @@
+import { html } from 'common-tags'
+import TypographyProse from './TypographyProse.vue'
+
+/**
+ * Style any vanilla HTML elements with Tailwindcss Typography `prose` classes.
+ *
+ * Read more about Tailwindcss Prose at https://github.com/tailwindlabs/tailwindcss-typography
+ */
+export default {
+  title: 'Foundation/Typography/With Prose',
+  component: TypographyProse,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  parameters: {
+    docs: {
+      source: {
+        code: html`
+          <div class="prose prose-neutral max-w-none dark:prose-invert">
+            <!-- vanilla HTML elements. -->
+          </div>
+        `,
+      },
+    },
+  },
+}
