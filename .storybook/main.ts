e84ee514edf0d95692850f8890712e6047552148
Achmad F. Ibrahim <acfatah@gmail.com>
2025-04-06 10:02:52 +0800

    chore: migrate storybook to the latest
    - installed by running `bunx storybook@latest upgrade`

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,15 +5,22 @@ const config: StorybookConfig = {
     // https://storybook.js.org/docs/configure/telemetry
     disableTelemetry: true,
   },
+
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-themes',
   ],
+
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
+  },
+
+  docs: {
+    autodocs: true,
   },
 }
 export default config
