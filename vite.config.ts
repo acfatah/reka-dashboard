6d9c21e56a7844f2959c6c53081afd6280fbfb8e
Achmad F. Ibrahim <acfatah@gmail.com>
2025-04-06 12:21:10 +0800

    feat: add vite-svg-loader library to load SVG image as vue component.
    see: https://github.com/jpkleemans/vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,14 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import svgLoader from 'vite-svg-loader'
 import { dirs as componentDirs, resolvers } from './src/components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    svgLoader(),
     AutoImport({
       dts: true,
       vueTemplate: true,
