1730f6420b864208fe5e68258713404933dbd77a
Achmad F. Ibrahim <acfatah@gmail.com>
2025-04-25 16:56:10 +0800

    feat: add Combobox Components

---
+++ src/components/ui/combobox/ComboboxInput.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import type { ComboboxInputEmits, ComboboxInputProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+import { ComboboxInput, useForwardPropsEmits } from 'reka-ui'
+import { computed } from 'vue'
+
+const props = defineProps<ComboboxInputProps & {
+  class?: HTMLAttributes['class']
+}>()
+
+const emits = defineEmits<ComboboxInputEmits>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
+
+<template>
+  <ComboboxInput
+    v-bind="forwarded"
+    :class="cn([
+      'flex h-9 w-full px-3 py-1',
+      'rounded-md border border-input bg-transparent text-sm shadow-sm',
+      'transition-colors',
+      'file:border-0 file:bg-transparent file:text-sm file:font-medium',
+      'placeholder:text-muted-foreground',
+      'focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none',
+      'disabled:cursor-not-allowed disabled:opacity-50',
+      props.class,
+    ])"
+  >
+    <slot />
+  </ComboboxInput>
+</template>
