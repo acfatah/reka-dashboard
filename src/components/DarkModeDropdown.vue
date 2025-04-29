e196cd4f57eb9ac81593664636e694e2f617f36e
Achmad F. Ibrahim <acfatah@gmail.com>
2025-04-29 08:05:30 +0800

    feat: add DarkModeDropdown and DarkModeToggle components

---
+++ src/components/DarkModeDropdown.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core'
+
+const mode = useColorMode()
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline" size="icon">
+        <Iconify icon="lucide:moon" class="size-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <Iconify icon="lucide:sun" class="absolute size-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem @click="mode = 'light'">
+        Light
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'dark'">
+        Dark
+      </DropdownMenuItem>
+      <DropdownMenuItem @click="mode = 'auto'">
+        System
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <Button
+    variant="outline"
+    class="group/toggle"
+    size="icon"
+    @click="toggleDark()"
+  >
+    <Iconify v-show="isDark" icon="lucide:sun" class="size-4" />
+    <Iconify v-show="!isDark" icon="lucide:moon" class="size-4" />
+    <span class="sr-only">Toggle theme</span>
+  </Button>
+</template>
