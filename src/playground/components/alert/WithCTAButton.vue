2ee0a0b0495dcb190727868272a3ce6543d8fc91
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-01 14:12:09 +0800

    fix(playground): incorrect class attribute name

--- src/playground/components/alert/WithCTAButton.vue
+++ src/playground/components/alert/WithCTAButton.vue
@@ -1,13 +1,13 @@
 <template>
   <Alert>
     <Iconify icon="lucide:alert-circle" class="size-4" />
-    <AlertTitle class-name="max-w-[calc(100%-4rem)] overflow-ellipsis">
+    <AlertTitle class="max-w-(100%-4rem) overflow-ellipsis">
       The selected emails have been marked as spam.
     </AlertTitle>
     <Button
       size="sm"
       variant="outline"
-      class-name="absolute top-2.5 right-3 h-6 shadow-none"
+      class="absolute top-2.5 right-3 h-6 shadow-none"
     >
       Undo
     </Button>
