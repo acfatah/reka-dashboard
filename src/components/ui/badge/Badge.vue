062573e71acd94025d19300f7aaaf3f1e9e1a3aa
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-02 10:11:42 +0800

    feat(Badge): update to shadcn v4

--- src/components/ui/badge/Badge.vue
+++ src/components/ui/badge/Badge.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
+import { Primitive } from 'reka-ui'
 import type { BadgeVariants } from '.'
 import { badgeVariants } from '.'
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   variant?: BadgeVariants['variant']
   class?: HTMLAttributes['class']
-}>()
+  as?: string
+  asChild?: boolean
+}>(), {
+  as: 'span',
+})
 </script>
 
 <template>
-  <div :class="cn(badgeVariants({ variant }), props.class)">
+  <Primitive
+    data-slot="badge"
+    :as="as"
+    :class="cn(badgeVariants({ variant }), props.class)"
+    :as-child="asChild"
+  >
     <slot />
-  </div>
+  </Primitive>
 </template>
