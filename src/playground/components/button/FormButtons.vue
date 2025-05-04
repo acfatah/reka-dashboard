5360c7a254b4e673acb588307b9139cfac99151a
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-04 11:52:17 +0800

    refactor(playground): improve layout responsiveness

--- src/playground/components/button/FormButtons.vue
+++ src/playground/components/button/FormButtons.vue
@@ -33,7 +33,7 @@ const onSubmit: SubmissionHandler<GenericObject> = function (values) {
 <template>
   <Form
     v-slot="{ meta }"
-    class="flex w-full flex-col space-y-6 sm:w-2/3"
+    class="flex w-full flex-col space-y-6 lg:w-3/4 xl:w-5/8 2xl:w-1/2 3xl:w-3/5"
     :validation-schema="formSchema"
     @submit="onSubmit"
   >
