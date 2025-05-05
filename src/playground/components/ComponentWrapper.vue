0fb1a7331898a4bf842e7c21df5598f45175dfc4
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-05 23:05:10 +0800

    fix: wrong class attribute name

--- src/playground/components/ComponentWrapper.vue
+++ src/playground/components/ComponentWrapper.vue
@@ -11,12 +11,12 @@ defineProps({
   <div
     class="flex w-full scroll-mt-16 flex-col rounded-lg border"
   >
-    <div className="border-b px-4 py-3">
-      <div className="text-sm font-medium">
+    <div class="border-b px-4 py-3">
+      <div class="text-sm font-medium">
         {{ name }}
       </div>
     </div>
-    <div className="flex flex-1 items-start gap-2 p-4">
+    <div class="flex flex-1 items-start gap-2 p-4">
       <slot />
     </div>
   </div>
