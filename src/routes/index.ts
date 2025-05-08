5473a10000d4ac86a5cabefbb598768a5263c094
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-09 07:42:56 +0800

    refactor: reorganize routes configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import type { RouteRecordRaw } from 'vue-router'
 import WelcomeLayout from '@/layouts/WelcomeLayout.vue'
 import HomeView from '@/views/Welcome/HomeView.vue'
 import { useNProgress } from '@vueuse/integrations/useNProgress'
@@ -5,29 +6,32 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const { start: startLoading, done: doneLoading } = useNProgress()
 
+export const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      layout: WelcomeLayout,
+    },
+  },
+
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('@/views/Welcome/AboutView.vue'),
+    meta: {
+      layout: WelcomeLayout,
+    },
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      meta: {
-        layout: WelcomeLayout,
-      },
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/Welcome/AboutView.vue'),
-      meta: {
-        layout: WelcomeLayout,
-      },
-    },
-  ],
+  routes,
 })
 
 router.beforeEach((_to, _from, next) => {
