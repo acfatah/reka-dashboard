0b25ab11f67762eb777a9a76b133d71ad4940c92
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-09 07:37:09 +0800

    refactor: rename router directory to routes

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import router from './router'
+import router from './routes'
 
 const app = createApp(App)
 
