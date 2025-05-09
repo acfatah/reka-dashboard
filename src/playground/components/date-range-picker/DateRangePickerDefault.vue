4168c0cf22c2244af7265f68c19d4d5cef261ba1
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-09 10:00:01 +0800

    refactor(playground): update date range picker default demo

--- src/playground/components/date-range-picker/DateRangePickerDefault.vue
+++ src/playground/components/date-range-picker/DateRangePickerDefault.vue
@@ -1,19 +1,52 @@
 <script setup lang="ts">
 import type { DateRange } from '@/components/ui/range-calendar'
-import type { Ref } from 'vue'
 import { RangeCalendar } from '@/components/ui/range-calendar'
-import { getLocalTimeZone, today } from '@internationalized/date'
-import { ref } from 'vue'
+import { DateFormatter, getLocalTimeZone, today } from '@internationalized/date'
+import { reactive } from 'vue'
 
 const start = today(getLocalTimeZone())
-const end = start.add({ days: 7 })
+const end = start.add({ days: 47 })
 
-const value = ref({
-  start,
-  end,
-}) as Ref<DateRange>
+const df = new DateFormatter('en-US', {
+  dateStyle: 'long',
+})
+
+const dateRange = reactive<DateRange>({ start, end })
+
+const formattedValue = computed(() => {
+  if (dateRange.start && dateRange.end)
+    return `${df.format(dateRange.start.toDate(getLocalTimeZone()))} - ${df.format(dateRange.end.toDate(getLocalTimeZone()))}`
+
+  return 'Pick a date'
+})
+
+function updateDateRange(value: DateRange) {
+  dateRange.start = value.start
+  dateRange.end = value.end
+}
 </script>
 
 <template>
-  <RangeCalendar v-model="value" class="w-fit rounded-md border" />
+  <Popover>
+    <PopoverTrigger as-child>
+      <Button
+        variant="outline"
+        :class="cn(
+          'w-fit justify-start text-left font-normal',
+          !dateRange && 'text-muted-foreground',
+        )"
+      >
+        <Icon icon="lucide:calendar" />
+        {{ formattedValue }}
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent class="w-auto p-0" align="start">
+      <RangeCalendar
+        :value="dateRange"
+        :number-of-months="2"
+        class="w-fit rounded-md border"
+        @update:model-value="updateDateRange"
+      />
+    </PopoverContent>
+  </Popover>
 </template>
