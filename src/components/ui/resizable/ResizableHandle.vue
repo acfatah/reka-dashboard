5b10f62382fe85173b430535b87fb07859f9b4e6
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-13 18:47:20 +0800

    chore(Resizable): update to shadcn v4

--- src/components/ui/resizable/ResizableHandle.vue
+++ src/components/ui/resizable/ResizableHandle.vue
@@ -3,8 +3,8 @@ import type { SplitterResizeHandleEmits, SplitterResizeHandleProps } from 'reka-
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 import { Icon } from '@iconify/vue'
+import { reactiveOmit } from '@vueuse/core'
 import { SplitterResizeHandle, useForwardPropsEmits } from 'reka-ui'
-import { computed } from 'vue'
 
 const props = defineProps<SplitterResizeHandleProps & {
   class?: HTMLAttributes['class']
@@ -12,28 +12,27 @@ const props = defineProps<SplitterResizeHandleProps & {
 }>()
 
 const emits = defineEmits<SplitterResizeHandleEmits>()
-
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
-  return delegated
-})
-
+const delegatedProps = reactiveOmit(props, 'class', 'withHandle')
 const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
 
 <template>
   <SplitterResizeHandle
+    data-slot="resizable-handle"
     v-bind="forwarded"
     :class="cn(
-      'relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2',
-      'focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:outline-none',
-      '[&[data-orientation=vertical]]:h-px [&[data-orientation=vertical]]:w-full [&[data-orientation=vertical]]:after:left-0 [&[data-orientation=vertical]]:after:h-1 [&[data-orientation=vertical]]:after:w-full [&[data-orientation=vertical]]:after:translate-x-0 [&[data-orientation=vertical]]:after:-translate-y-1/2 [&[data-orientation=vertical]>div]:rotate-90',
+      'relative flex w-px items-center justify-center',
+      'bg-border',
+      'after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2',
+      'focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:outline-hidden',
+      ' data-[orientation=vertical]:h-px data-[orientation=vertical]:w-full data-[orientation=vertical]:after:left-0 data-[orientation=vertical]:after:h-1 data-[orientation=vertical]:after:w-full data-[orientation=vertical]:after:translate-x-0 data-[orientation=vertical]:after:-translate-y-1/2',
+      ' [&[data-orientation=vertical]>div]:rotate-90',
       props.class,
     )"
   >
     <template v-if="props.withHandle">
-      <div class="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
-        <Icon icon="radix-icons:drag-handle-dots-2" />
+      <div class="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
+        <Icon icon="lucide:grip-vertical" class="size-2.5" />
       </div>
     </template>
   </SplitterResizeHandle>
--- src/components/ui/resizable/ResizablePanelGroup.vue
+++ src/components/ui/resizable/ResizablePanelGroup.vue
@@ -2,25 +2,24 @@
 import type { SplitterGroupEmits, SplitterGroupProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
+import { reactiveOmit } from '@vueuse/core'
 import { SplitterGroup, useForwardPropsEmits } from 'reka-ui'
-import { computed } from 'vue'
 
-const props = defineProps<SplitterGroupProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<SplitterGroupProps & {
+  class?: HTMLAttributes['class']
+}>()
+
 const emits = defineEmits<SplitterGroupEmits>()
-
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
-  return delegated
-})
-
+const delegatedProps = reactiveOmit(props, 'class')
 const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
 
 <template>
   <SplitterGroup
+    data-slot="resizable-panel-group"
     v-bind="forwarded"
     :class="cn(
-      'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
+      'flex h-full w-full data-[orientation=vertical]:flex-col',
       props.class,
     )"
   >
