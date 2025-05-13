74c0b5caa5171fb27c5767e0a84577859117fbbc
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-14 06:48:54 +0800

    chore(Popover): update to shadcn v4

--- src/components/ui/popover/PopoverContent.vue
+++ src/components/ui/popover/PopoverContent.vue
@@ -2,8 +2,8 @@
 import type { PopoverContentEmits, PopoverContentProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
+import { reactiveOmit } from '@vueuse/core'
 import { PopoverContent, PopoverPortal, useForwardPropsEmits } from 'reka-ui'
-import { computed } from 'vue'
 
 defineOptions({
   inheritAttrs: false,
@@ -17,23 +17,19 @@ const props = withDefaults(defineProps<PopoverContentProps & {
 })
 
 const emits = defineEmits<PopoverContentEmits>()
-
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
-
-  return delegated
-})
-
+const delegatedProps = reactiveOmit(props, 'class')
 const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
 
 <template>
   <PopoverPortal>
     <PopoverContent
+      data-slot="popover-content"
       v-bind="{ ...forwarded, ...$attrs }"
       :class="cn(
-        'z-50 w-72 rounded-md border p-4 shadow-md outline-none',
-        'bg-popover text-popover-foreground',
+        'z-50 w-72 origin-(--reka-popover-content-transform-origin) p-4',
+        'rounded-md border bg-popover shadow-md outline-hidden',
+        'text-popover-foreground',
         'data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95',
         'data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95',
