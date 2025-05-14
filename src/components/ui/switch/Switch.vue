103424422414b6524cc0ea956134a988954545d9
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-14 21:01:35 +0800

    chore(Switch): update to shadcn v4

--- src/components/ui/switch/Switch.vue
+++ src/components/ui/switch/Switch.vue
@@ -2,42 +2,45 @@
 import type { SwitchRootEmits, SwitchRootProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
+import { reactiveOmit } from '@vueuse/core'
 import {
   SwitchRoot,
   SwitchThumb,
   useForwardPropsEmits,
 } from 'reka-ui'
-import { computed } from 'vue'
 
-const props = defineProps<SwitchRootProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<SwitchRootProps & {
+  class?: HTMLAttributes['class']
+}>()
+
 const emits = defineEmits<SwitchRootEmits>()
-
-const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
-
-  return delegated
-})
-
+const delegatedProps = reactiveOmit(props, 'class')
 const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
 
 <template>
   <SwitchRoot
+    data-slot="switch"
     v-bind="forwarded"
     :class="cn(
-      'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm',
-      'focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background focus-visible:outline-none',
-      'transition-colors',
-      'data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+      'peer inline-flex h-[1.15rem] w-8 shrink-0 items-center',
+      'rounded-full border border-transparent shadow-xs outline-none',
+      'transition-all',
+      'focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50',
       'disabled:cursor-not-allowed disabled:opacity-50',
+      'data-[state=checked]:bg-primary',
+      'data-[state=unchecked]:bg-input dark:data-[state=unchecked]:bg-input/80',
       props.class,
     )"
   >
     <SwitchThumb
+      data-slot="switch-thumb"
       :class="cn(
-        'pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0',
+        'pointer-events-none block size-4',
+        'rounded-full bg-background ring-0',
         'transition-transform',
-        'data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0',
+        'data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0',
+        'dark:data-[state=checked]:bg-primary-foreground dark:data-[state=unchecked]:bg-foreground',
       )"
     >
       <slot name="thumb" />
