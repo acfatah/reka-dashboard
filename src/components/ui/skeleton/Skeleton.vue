535771091aa43cd2850b811f5d98f396fe5209f2
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-14 20:41:46 +0800

    chore(Skeleton): update to shadcn v4

--- src/components/ui/skeleton/Skeleton.vue
+++ src/components/ui/skeleton/Skeleton.vue
@@ -2,11 +2,19 @@
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 
-interface SkeletonProps { class?: HTMLAttributes['class'] }
+interface SkeletonProps {
+  class?: HTMLAttributes['class']
+}
 
 const props = defineProps<SkeletonProps>()
 </script>
 
 <template>
-  <div :class="cn('animate-pulse rounded-md bg-primary/10', props.class)" />
+  <div
+    data-slot="skeleton"
+    :class="cn(
+      'animate-pulse rounded-md bg-primary/10',
+      props.class,
+    )"
+  />
 </template>
