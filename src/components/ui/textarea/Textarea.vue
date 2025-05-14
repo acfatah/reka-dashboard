8bc1ee40eb4eb72ae32b624face0e8fef6eaa561
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-14 21:08:34 +0800

    chore(Textarea): update to shadcn v4

--- src/components/ui/textarea/Textarea.vue
+++ src/components/ui/textarea/Textarea.vue
@@ -22,11 +22,19 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 <template>
   <textarea
     v-model="modelValue"
+    data-slot="textarea"
     :class="cn(
-      'flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 shadow-sm',
-      'text-sm placeholder:text-muted-foreground ',
-      'focus-visible:ring-1 focus-visible:ring-ring focus-visible:outline-none',
+      'flex field-sizing-content min-h-16 w-full px-3 py-2',
+      'rounded-md border border-input bg-transparent',
+      'shadow-xs outline-none',
+      'text-base',
+      'transition-[color,box-shadow]',
+      'placeholder:text-muted-foreground',
+      'focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50',
+      'aria-invalid:border-destructive aria-invalid:ring-destructive/20',
       'disabled:cursor-not-allowed disabled:opacity-50',
+      'dark:bg-input/30 dark:aria-invalid:ring-destructive/40',
+      'md:text-sm',
       props.class,
     )"
   />
