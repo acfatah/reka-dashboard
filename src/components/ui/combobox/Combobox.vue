88d2650c61fb4a6a9d34cb4aac597103ae14ff72
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-21 07:36:08 +0800

    feat(Combobox): expose open state

--- src/components/ui/combobox/Combobox.vue
+++ src/components/ui/combobox/Combobox.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
 import type { ComboboxRootEmits, ComboboxRootProps } from 'reka-ui'
 import { ComboboxRoot, useForwardPropsEmits } from 'reka-ui'
+import { ref } from 'vue'
 
 const props = defineProps<ComboboxRootProps>()
 const emits = defineEmits<ComboboxRootEmits>()
 const forwarded = useForwardPropsEmits(props, emits)
+const open = ref(false)
+
+function setOpen(value: boolean) {
+  open.value = value
+}
 </script>
 
 <template>
   <ComboboxRoot
-    v-slot="{ open }"
-    data-slot="combobox"
     v-bind="forwarded"
+    v-model:open="open"
+    data-slot="combobox"
   >
-    <slot v-bind="{ open }" />
+    <slot v-bind="{ open, setOpen }" />
   </ComboboxRoot>
 </template>
