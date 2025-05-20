ae30d6642d0fa8d8f978ed21cf070787ab563e1b
Achmad F. Ibrahim <acfatah@gmail.com>
2025-05-21 07:36:31 +0800

    feat(Popover): expose open state

--- src/components/ui/popover/Popover.vue
+++ src/components/ui/popover/Popover.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
 import type { PopoverRootEmits, PopoverRootProps } from 'reka-ui'
 import { PopoverRoot, useForwardPropsEmits } from 'reka-ui'
+import { ref } from 'vue'
 
 const props = defineProps<PopoverRootProps>()
 const emits = defineEmits<PopoverRootEmits>()
 const forwarded = useForwardPropsEmits(props, emits)
+const open = ref(false)
+
+function setOpen(value: boolean) {
+  open.value = value
+}
 </script>
 
 <template>
   <PopoverRoot
-    data-slot="popover"
     v-bind="forwarded"
+    v-model:open="open"
+    data-slot="popover"
   >
-    <slot />
+    <slot v-bind="{ open, setOpen }" />
   </PopoverRoot>
 </template>
